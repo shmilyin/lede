5f334aa90f00e112c8b6b737ab43165e0a72aa07
shmilyin <351140724@qq.com>
2020-10-28 01:21:53 -0700

    some modify

--- target/linux/ar71xx/image/tiny-tp-link.mk
+++ target/linux/ar71xx/image/tiny-tp-link.mk
@@ -340,7 +340,7 @@ endef
 TARGET_DEVICES += tl-wr1041n-v2
 
 define Device/tl-wr703n-v1
-  $(Device/tplink-4mlzma)
+  $(Device/tplink-16mlzma)
   DEVICE_TITLE := TP-LINK TL-WR703N v1
   DEVICE_PACKAGES := kmod-usb2
   BOARDNAME := TL-WR703N
